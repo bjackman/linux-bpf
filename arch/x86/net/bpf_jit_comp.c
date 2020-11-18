ee6d074bf48db085a8e38227251586776b3c589b
Brendan Jackman <bhenryj0117@gmail.com>
2020-11-18 15:57:11 +0100

    bpf: Add instructions for atomic_cmpxchg and friends
    These are the operations that implement atomic exchange and
    compare-exchange.
    
    They are peculiarly named because of the presence of the separate
    FETCH field that tells you whether the instruction writes the value
    back to the src register.
    
    BPF_CMP is supported without BPF_FETCH, providing atomic_set. However
    BPF_CMPSET without BPF_FETCH (i.e. an atomic compare-and-set without
    knowing whether the write was successfuly) isn't implemented by the
    kernel, x86, or ARM. It would be a burden on the JIT and it's hard to
    imagine a use for this operation, so it's not supported.
    
    There are two significant design decisions made for the CMPSET
    instruction:
    
     - To solve the issue that this operation fundamentally has 3
       operands, but we only have two register fields. Therefore the
       operand we compare against (the kernel's API calls it 'old') is
       hard-coded to be R0. x86 has similar design (and A64 doesn't
       have this problem).
    
       A potential alternative might be to encode the other operand's
       register number in the immediate field.
    
     - The kernel's atomic_cmpxchg returns the old value, while the C11
       userspace APIs return a boolean indicating the comparison
       result. Which should BPF do? A64 returns the old value. x86 returns
       the old value in the hard-coded register (and also sets a
       flag). That means return-old-value is easier to JIT.
    
    Signed-off-by: Brendan Jackman <jackmanb@google.com>
    Change-Id: I3f19ad867dfd08515eecf72674e6fdefe28424bb

--- arch/x86/net/bpf_jit_comp.c
+++ arch/x86/net/bpf_jit_comp.c
@@ -1255,16 +1255,28 @@ st:			if (is_imm8(insn->off))
 
 		case BPF_STX | BPF_ATOMIC | BPF_W:
 		case BPF_STX | BPF_ATOMIC | BPF_DW:
-			if (BPF_OP(insn->imm) != BPF_ADD) {
-				pr_err("bpf_jit: unknown opcode %02x\n", insn->imm);
-				return -EFAULT;
+			if (insn->imm == BPF_SET) {
+				/*
+				 * atomic_set((u32/u64*)(dst_reg + off), src_reg);
+				 * On x86 atomic_set is just WRITE_ONCE.
+				 */
+				emit_stx(&prog, BPF_SIZE(insn->code), dst_reg, src_reg, insn->off);
+				break;
+			}
+
+			if (insn->imm == BPF_CMPSET) {
+				/*
+				 * x86 cmpxchg clobbers rax but this insn
+				 * shouldn't touch R0, so save it.
+				 */
+				EMIT_mov(AUX_REG, BPF_REG_0);
 			}
 
 			EMIT1(0xF0); /* lock prefix */
 
 			/* emit REX if necessary */
 			if (BPF_SIZE(insn->code) == BPF_W) {
-				/* Emit 'lock add dword ptr [rax + off], eax' */
+				/* Addressing like 'dword ptr [rax + off], eax' */
 				if (is_ereg(dst_reg) || is_ereg(src_reg))
 					EMIT1(add_2mod(0x40, dst_reg, src_reg));
 			} else {
@@ -1272,15 +1284,30 @@ st:			if (is_imm8(insn->off))
 			}
 
 			/* emit opcode */
-			if (insn->imm & BPF_FETCH) {
+			switch (insn->imm) {
+			case BPF_SET | BPF_FETCH:
+				/* src_reg = atomic_chg(*(u32/u64*)(dst_reg + off), src_reg); */
+				EMIT1(0x87);
+				break;
+			case BPF_CMPSET | BPF_FETCH:
+				/* r0 = atomic_cmpxchg(*(u32/u64*)(dst_reg + off), r0, src_reg); */
+				EMIT2(0x0F, 0xB1);
+				break;
+			case BPF_ADD | BPF_FETCH:
 				/* src_reg = sync_fetch_and_add(*(dst_reg + off), src_reg); */
 				EMIT2(0x0F, 0xC1);
-			} else {
+				break;
+			case BPF_ADD:
 				/* lock *(u32/u64*)(dst_reg + off) += src_reg */
 				EMIT1(0x01);
+				break;
+			default:
+				pr_err("bpf_jit: unknown atomic opcode %02x\n", insn->imm);
+				return -EFAULT;
 			}
 
 			emit_modrm_dstoff(&prog, dst_reg, src_reg, insn->off);
+
 			break;
 
 			/* call */
