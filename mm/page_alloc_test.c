01e6a35d5f8549ab6595351a08c43f864533fc7b
Brendan Jackman <jackmanb@google.com>
2024-05-17 01:09:28 +0200

    WIP on getting memory block
    I got distracted by the locking biz. It looks like
    https://lore.kernel.org/all/20210531093958.15021-1-osalvador@suse.de/T/#u
    got applied as 27cacaad16c54 even though there was a v2:
    https://lore.kernel.org/linux-mm/20210602091457.17772-3-osalvador@suse.de/

--- mm/page_alloc_test.c
+++ mm/page_alloc_test.c
@@ -1,4 +1,5 @@
 #include <linux/gfp.h>
+#include <linux/memory.h>
 
 #include <kunit/test.h>
 
@@ -6,9 +7,40 @@ static void test_alloc(struct kunit *test)
 {
 	struct page *page = __alloc_pages_zonelist(
 		GFP_KERNEL, 0, node_zonelist(0, GFP_KERNEL), NULL);
+	struct pglist_data *node0 = NODE_DATA(0);
+	struct zone *zone = &NODE_DATA(0)->node_zones[ZONE_MOVABLE];
+	unsigned long pfn;
+	unsigned seq;
 
-	KUNIT_ASSERT_NOT_NULL(test, page);
-	__free_pages(page, 0);
+	// Carve out a fake micro-node whose zones are not on any real
+	// zonelists. We can then allocate from and free to this node for
+	// testing, without the rest of the system interfering.
+
+	// Figure out which memory blocks will go into our fake node. We
+	// arbitrarily pick node 0 and grab half of ZONE_MOVABLE since we can
+	// get that for ourselves.
+	do {
+		seq = zone_span_seqbegin(zone);
+		spanned_pages = zone->spanned_pages;
+	} while (zone_span_seqretry(zone, seq));
+	struct memory_block *block_start = find_memory_block(
+		pfn_to_section_nr(zone->zone_start_pfn));
+
+	KUNIT_ASSERT_NOT_NULL(test, block_start);
+	printk("Found block, node %d\n", block_start->nid);
+
+
+	// TODO:
+	// Figure out a range of addrresses to carve out for the separated
+	// allocator. I guess we cannot carve this range from the middle of an
+	// existing zone?.
+	// find  corresponding memory blocks with
+	// find_memory_block(pfn_to_section_nr(something)), then call
+	// memory_block_offline to remove it Then I tihnk we might want
+	// add_hotplug_memory_block (assuming the previous step removed the
+	// memory_block device) then memory_block_add_nid to associate with a
+	// fake NID?
+
 }
 
 static struct kunit_case test_cases[] = {
